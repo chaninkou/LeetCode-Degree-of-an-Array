552458f9f85fdef1075f0c8e0dac1d27f8c41e24
chaninkou <michaelkoulove@yahoo.com>
2019-07-22 20:43:05 -0700

    Not that good problem. Need to review this.

---
+++ src/degree/Main.java
@@ -0,0 +1,52 @@
+package degree;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) {
+		// Not that good of a problem, need to review this
+		int[] nums = {1, 2, 2, 3, 1};
+		
+		System.out.println("Input: " + Arrays.toString(nums));
+		
+		System.out.println("Solution: " + findShortestSubArray(nums));
+	}
+	
+	// Not that good problem
+    public static int findShortestSubArray(int[] nums) {
+        if(nums.length == 0 || nums == null){
+            return 0;
+        }
+        
+        Map<Integer, int[]> map = new HashMap();
+        
+        for(int i = 0; i < nums.length; i++){
+            if(!map.containsKey(nums[i])){
+                // first element in array is degree
+                // second is the first index of this key
+                // third is the last index of this key
+                map.put(nums[i], new int[]{1,i,i});
+            }else{
+                int[] temp = map.get(nums[i]);
+                temp[0] = temp[0] + 1;
+                temp[2] = i;
+            }
+        }
+        
+        int degree = Integer.MIN_VALUE;
+
+        int res = Integer.MAX_VALUE;
+        
+        for(int[] value : map.values()){
+            if(value[0] > degree){
+                degree = value[0];
+                res = value[2] - value[1] + 1;
+            } else if (value[0] == degree){
+                res = Math.min(value[2] - value[1] + 1, res);
+            }
+        }
+        return res;
+    }
+}
